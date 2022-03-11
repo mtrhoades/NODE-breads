430e3c23ecba6b74663c954478546d83baf67805
root <mtrhoades0727@gmail.com>
2022-03-10 17:16:38 -1000

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,18 +3,22 @@ const breads = express.Router()
 const Bread = require('../models/bread.js')
 
 // INDEX
-breads.get('/', (req, res) => {
-    res.render('index',
-      {
-        breads: Bread,
-        title: 'Index Page'
-      }
-    )
-})
+breads.get("/", (req, res) => {
+  res.render("index", {
+    breads: Bread,
+    title: "Index Page",
+  });
+});
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-})
+breads.get("/:arrayIndex", (req, res) => {
+  if (Bread[req.params.arrayIndex]) {
+    res.render("show", {
+      bread: Bread[req.params.arrayIndex],
+    });
+  } else {
+    res.render("error404");
+  }
+});
 
-module.exports = breads
+module.exports = breads;
--- views/index.jsx
+++ views/index.jsx
@@ -1,26 +1,19 @@
-const React = require('react')
-const Default = require('./layouts/default')
+const React = require("react");
+const Default = require("./layouts/default");
 
-function Index ({breads, title})  {
-    return (
-      <Default title={title}>
-        {/* <h2>Index Page</h2> */}
-        {/* <p>I have {breads[0].name} bread!</p> */}
-        <ul>
-            {
-                breads.map((bread, index) => {
-                    return (
-                        <li key={index}>
-                            <a href={`./breads/${index}`}>
-                                {bread.name}
-                            </a>
-                        </li>
-                    )
-                })
-            }
-        </ul>
-      </Default>
-    )
+function Index({ breads }) {
+  return (
+    <Default title={'title'}>
+      <h2>Index Page</h2>
+      {breads.map((bread, index) => {
+        return (
+          <li key={index}>
+            <a href={`/breads/${index}`}>{bread.name}</a>
+          </li>
+        );
+      })}
+    </Default>
+  );
 }
 
-module.exports = Index
+module.exports = Index;
