a18e147e597122f50e071c3419842d1c0d69d687
root <mtrhoades0727@gmail.com>
2022-03-13 10:29:18 -1000

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -10,23 +10,6 @@ breads.get("/", (req, res) => {
   });
 });
 
-// NEW
-breads.get('/new', (req, res) => {
-  res.render('new')
-});
-
-// SHOW
-breads.get("/:arrayIndex", (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render("show", {
-      bread: Bread[req.params.arrayIndex],
-      index: req.params.arrayIndex,
-    });
-  } else {
-    res.render("error404");
-  }
-});
-
 // CREATE
 breads.post('/', (req, res) => {
   if (!req.body.image) {
@@ -41,11 +24,50 @@ breads.post('/', (req, res) => {
   res.redirect('/breads')
 });
 
+// NEW
+breads.get('/new', (req, res) => {
+  res.render('new')
+});
+
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+});
+
+// SHOW
+breads.get("/:arrayIndex", (req, res) => {
+  if (Bread[req.params.arrayIndex]) {
+    res.render("show", {
+      bread: Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
+    });
+  } else {
+    res.render("error404");
+  }
+});
+
+
 // DELETE
 breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
 });
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+});
+
+
 
 module.exports = breads;
 
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method'))
+app.use(methodOverride('methodOverride'))
 
 // ROUTES
 app.get('/', (req, res) => {
